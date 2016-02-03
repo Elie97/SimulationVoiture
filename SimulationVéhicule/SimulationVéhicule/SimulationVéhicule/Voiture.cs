5cedfef534c5c1b80aa5d0275eb98f93d0143a44
Elie97 <lamoureuxelie@gmail.com>
2016-02-03 11:16:25 -0500

    Freinage + Reculons + Début Tourner
    C'est ça là!

--- SimulationVéhicule/SimulationVéhicule/SimulationVéhicule/Voiture.cs
+++ SimulationVéhicule/SimulationVéhicule/SimulationVéhicule/Voiture.cs
@@ -8,10 +8,13 @@ namespace SimulationVéhicule
 {
     public class Voiture : Microsoft.Xna.Framework.DrawableGameComponent
     {
-        const float DÉCÉLÉRATION = 0.002f;// en décimètre
+        const float DÉCÉLÉRATION = 0.005f;// en décimètre
         const float VITESSE_MAX = 100.0f;// Km/H
+        const float VITESSE_MAX_RECULONS = -30.0f;// Km/H
         const float ACCÉLÉRATION = 4.0f;// Temps en seconde pour atteindre 100 km/h
+        const float ACCÉLÉRATION_RECULONS = -2.0f;// Temps en seconde pour atteindre 100 km/h
         const float MASSE_VOITURE = 1000.0f;// En Kilogramme
+        const float ACCÉLÉRATION_FREIN = 4.0f;// Km/h Valeur pas correcte
 
 
         string NomModèle { get; set; }
@@ -34,6 +37,8 @@ namespace SimulationVéhicule
         public float Vitesse { get; set; }
         int CPT { get; set; }
 
+        int Déplacement { get; set; }
+
         public Voiture(Game jeu, String nomModèle, float échelleInitiale, Vector3 rotationInitiale, Vector3 positionInitiale, float intervalleMAJ)
             : base(jeu)
         {
@@ -54,6 +59,8 @@ namespace SimulationVéhicule
             D3Actif = false;
             Vitesse = 0;
             CPT = 0;
+            Déplacement = 0;
+            Mouse.SetPosition(Game.Window.ClientBounds.Width / 2, Game.Window.ClientBounds.Height / 2);
             base.Initialize();
         }
 
@@ -79,7 +86,7 @@ namespace SimulationVéhicule
         {
             foreach (ModelMesh maille in Modèle.Meshes)
             {
-                Game.Window.Title = Convert.ToInt32(PixelToKMH(Vitesse)).ToString();
+                Game.Window.Title = GestionInput.GetPositionSouris().ToString() + " Déplacement : " + Déplacement.ToString() + " CPT : " + CPT.ToString();
                 Matrix mondeLocal = TransformationsModèle[maille.ParentBone.Index] * GetMonde();
                 foreach (ModelMeshPart portionDeMaillage in maille.MeshParts)
                 {
@@ -98,10 +105,154 @@ namespace SimulationVéhicule
             D1Actif = GestionToucheActive(Keys.D1, D1Actif);
             D2Actif = GestionToucheActive(Keys.D2, D2Actif);
             D3Actif = GestionToucheActive(Keys.D3, D3Actif);
+            Mouse.SetPosition(GestionInput.GetPositionSouris().X, Game.Window.ClientBounds.Height / 2);
 
             TempsÉcouléDepuisMAJ += (float)gameTime.ElapsedGameTime.TotalSeconds;
             if (TempsÉcouléDepuisMAJ >= IntervalleMAJ)
             {
+                if (GestionInput.EstEnfoncée(Keys.W))
+                {
+                    if (GestionInput.EstEnfoncée(Keys.Up))
+                    {
+                        Accélération(true);
+                    }
+                    else if (GestionInput.EstEnfoncée(Keys.Down))
+                    {
+                        Accélération(false);
+                    }
+                    Avance();   
+                }
+                else
+                {
+                    Décélération();
+                }
+
+                if (GestionInput.EstEnfoncée(Keys.Tab))
+                {
+                    Freinage();
+                }
+
+                GetDéplacementSouris();
+
+                CalculerMonde();
+                TempsÉcouléDepuisMAJ = 0;
+            }
+            base.Update(gameTime);
+        }
+
+        public virtual Matrix GetMonde()
+        {
+            return Monde;
+        }
+
+        public bool GestionToucheActive(Keys key, bool estActif)
+        {
+            if (GestionInput.EstNouvelleTouche(key))
+            {
+                estActif = !estActif;
+            }
+            return estActif;
+        }
+
+        public void Avance()
+        {
+            Position = new Vector3(Position.X, Position.Y, Position.Z + Vitesse);
+        }
+
+        void Décélération()
+        {
+            if (Vitesse > 0)
+            {
+                Vitesse -= DÉCÉLÉRATION;
+            }
+            else
+            {
+                Vitesse = 0;
+            }
+        }
+
+        void Accélération(bool positive)
+        {
+            if (positive == true)
+            {
+                if (Vitesse < KMHtoPixel(VITESSE_MAX))
+                {
+                    Vitesse += ((KMHtoPixel(VITESSE_MAX) / ACCÉLÉRATION) / 60f);
+                }
+                else
+                {
+                    Vitesse = KMHtoPixel(VITESSE_MAX);
+                }   
+            }
+            else
+            {
+                if (Vitesse > KMHtoPixel(VITESSE_MAX_RECULONS))
+                {
+                    Vitesse -= ((KMHtoPixel(VITESSE_MAX_RECULONS) / ACCÉLÉRATION_RECULONS) / 60f);
+                }
+            }
+        }
+
+        void Freinage()
+        {
+            if (Vitesse > 0)
+            {
+                Vitesse -= (ACCÉLÉRATION_FREIN) / 60f;
+            }
+            else if (Vitesse < 0)
+            {
+                Vitesse += (ACCÉLÉRATION_FREIN) / 60f;
+            }
+            else
+            {
+                Vitesse = 0;
+            }
+        }
+
+        float KMHtoPixel(float kmh)
+        {
+            return (kmh / 3.6f) / 6;
+        }
+
+        float PixelToKMH(float pixel)
+        {
+            return (pixel * 6) * 3.6f;
+        }
+
+        void GetDéplacementSouris()
+        {
+            int positionInitiale = 0;
+            int positionFinale = 0;
+            positionInitiale = GestionInput.GetPositionSouris().X;
+            if (GestionInput.EstAncienClicGauche())
+            {
+                //positionInitiale = GestionInput.GetPositionSouris().X;
+                //if (GestionInput.EstAncienClicGauche())
+                //{
+                    //positionFinale = GestionInput.GetPositionSouris().X;
+                //}
+                CPT++;
+            }
+            else
+            {
+                positionFinale = GestionInput.GetPositionSouris().X;
+                CPT = 0;
+            }
+
+            if (CPT == 0)
+            {
+                positionFinale = GestionInput.GetPositionSouris().X;
+            }
+            else
+            {
+                positionFinale = positionInitiale;
+            }
+            Déplacement = positionFinale;
+        }
+    }
+}
+
+
                 //if (D2Actif)
                 //{
                 //    Rotation = new Vector3(Rotation.X + 0.05f, Rotation.Y, Rotation.Z);//Pas cette valeur, check msg colnet ;)
@@ -140,77 +291,3 @@ namespace SimulationVéhicule
                 //{
                 //    Recule();
                 //}
-                if (GestionInput.EstEnfoncée(Keys.W))
-                {
-                    Accélération();
-                }
-                else
-                {
-                    Décélération();
-                }
-                Avance();
-                CalculerMonde();
-                TempsÉcouléDepuisMAJ = 0;
-            }
-            base.Update(gameTime);
-        }
-
-        public virtual Matrix GetMonde()
-        {
-            return Monde;
-        }
-
-        public bool GestionToucheActive(Keys key, bool estActif)
-        {
-            if (GestionInput.EstNouvelleTouche(key))
-            {
-                estActif = !estActif;
-            }
-            return estActif;
-        }
-
-        public void Avance()
-        {
-            //Vitesse += 1;
-            Position = new Vector3(Position.X, Position.Y, Position.Z + Vitesse);
-        }
-        public void Recule()
-        {
-            Position = new Vector3(Position.X, Position.Y, Position.Z - Vitesse);
-        }
-
-        void Décélération()
-        {
-            if (Vitesse > 0)
-            {
-                Vitesse -= DÉCÉLÉRATION;//mettons
-            }
-            else
-            {
-                Vitesse = 0;
-            }
-        }
-
-        void Accélération()
-        {
-            if (Vitesse < KMHtoPixel(VITESSE_MAX))
-            {
-                Vitesse += ((KMHtoPixel(VITESSE_MAX) / ACCÉLÉRATION) / 60f);
-            }
-            else
-            {
-                Vitesse = KMHtoPixel(VITESSE_MAX);
-            }
-        }
-
-        float KMHtoPixel(float kmh)
-        {
-            return (kmh / 3.6f) / 6;
-        }
-
-        float PixelToKMH(float pixel)
-        {
-            return (pixel * 6) * 3.6f;
-        }
-    }
-}
